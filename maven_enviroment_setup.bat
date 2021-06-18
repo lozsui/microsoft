3fb91d5c4bf7f37a7ced9670f12002e84eb5959a
Samuel Bächler <samuel.baechler@ewz.ch>
2021-06-18 16:53:04 +0200

    environment variables for mvn

---
+++ maven_enviroment_setup.bat
@@ -0,0 +1,8 @@
+SET PATH=C:\Windows\System32
+
+SET "JAVA_HOME=C:\GNOT\java\open-jdk-16.0.1"
+SET PATH=%PATH%;%JAVA_HOME%\bin
+
+SET "MAVEN_HOME=C:\GNOT\apache-maven\apache-maven-3.8.1"
+SET PATH=%PATH%;%MAVEN_HOME%\bin
+
